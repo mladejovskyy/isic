35237b9c214230b4cca3dae4a361f8e886b0802d
mladejovskyy <116684151+mladejovskyy@users.noreply.github.com>
2023-04-25 14:05:51 +0200

    Add files via upload

---
+++ isic/isic/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace isic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_jmeno = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_telefon = new System.Windows.Forms.TextBox();
+            this.textBox_prijmeni = new System.Windows.Forms.TextBox();
+            this.button_jmeno_prijmeni = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_cena = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_pojisteni = new System.Windows.Forms.TextBox();
+            this.button_castka = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox_vypis_cele_jmeno = new System.Windows.Forms.TextBox();
+            this.textBox_vypis_telefon = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox_vypis_celkova_cena = new System.Windows.Forms.TextBox();
+            this.button_register = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox_cena2 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox_pojisteni2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(59, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRŮKAZ ISIC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jméno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefon";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Příjmení";
+            // 
+            // textBox_jmeno
+            // 
+            this.textBox_jmeno.Location = new System.Drawing.Point(121, 87);
+            this.textBox_jmeno.Name = "textBox_jmeno";
+            this.textBox_jmeno.Size = new System.Drawing.Size(156, 20);
+            this.textBox_jmeno.TabIndex = 5;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(121, 134);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(357, 20);
+            this.textBox_email.TabIndex = 6;
+            // 
+            // textBox_telefon
+            // 
+            this.textBox_telefon.Location = new System.Drawing.Point(121, 176);
+            this.textBox_telefon.Name = "textBox_telefon";
+            this.textBox_telefon.Size = new System.Drawing.Size(357, 20);
+            this.textBox_telefon.TabIndex = 7;
+            // 
+            // textBox_prijmeni
+            // 
+            this.textBox_prijmeni.Location = new System.Drawing.Point(348, 87);
+            this.textBox_prijmeni.Name = "textBox_prijmeni";
+            this.textBox_prijmeni.Size = new System.Drawing.Size(130, 20);
+            this.textBox_prijmeni.TabIndex = 8;
+            // 
+            // button_jmeno_prijmeni
+            // 
+            this.button_jmeno_prijmeni.Location = new System.Drawing.Point(503, 85);
+            this.button_jmeno_prijmeni.Name = "button_jmeno_prijmeni";
+            this.button_jmeno_prijmeni.Size = new System.Drawing.Size(102, 23);
+            this.button_jmeno_prijmeni.TabIndex = 10;
+            this.button_jmeno_prijmeni.Text = "Odeslat";
+            this.button_jmeno_prijmeni.UseVisualStyleBackColor = true;
+            this.button_jmeno_prijmeni.Click += new System.EventHandler(this.button_jmeno_prijmeni_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Location = new System.Drawing.Point(59, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "UHRAZENÁ ČÁSTKA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 288);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Cena průkazu";
+            // 
+            // textBox_cena
+            // 
+            this.textBox_cena.Location = new System.Drawing.Point(138, 285);
+            this.textBox_cena.Name = "textBox_cena";
+            this.textBox_cena.Size = new System.Drawing.Size(130, 20);
+            this.textBox_cena.TabIndex = 13;
+            this.textBox_cena.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(281, 288);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Pojištění průkazu";
+            // 
+            // textBox_pojisteni
+            // 
+            this.textBox_pojisteni.Location = new System.Drawing.Point(376, 285);
+            this.textBox_pojisteni.Name = "textBox_pojisteni";
+            this.textBox_pojisteni.Size = new System.Drawing.Size(102, 20);
+            this.textBox_pojisteni.TabIndex = 15;
+            this.textBox_pojisteni.Text = "0";
+            // 
+            // button_castka
+            // 
+            this.button_castka.Location = new System.Drawing.Point(503, 325);
+            this.button_castka.Name = "button_castka";
+            this.button_castka.Size = new System.Drawing.Size(102, 23);
+            this.button_castka.TabIndex = 16;
+            this.button_castka.Text = "Odeslat";
+            this.button_castka.UseVisualStyleBackColor = true;
+            this.button_castka.Click += new System.EventHandler(this.button_castka_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label9.Location = new System.Drawing.Point(59, 392);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(143, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "REKAPITULACE";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(59, 429);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Jméno a příjmení";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(294, 429);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(89, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Kontaktní telefon";
+            // 
+            // textBox_vypis_cele_jmeno
+            // 
+            this.textBox_vypis_cele_jmeno.Location = new System.Drawing.Point(62, 456);
+            this.textBox_vypis_cele_jmeno.Name = "textBox_vypis_cele_jmeno";
+            this.textBox_vypis_cele_jmeno.Size = new System.Drawing.Size(215, 20);
+            this.textBox_vypis_cele_jmeno.TabIndex = 20;
+            // 
+            // textBox_vypis_telefon
+            // 
+            this.textBox_vypis_telefon.Location = new System.Drawing.Point(297, 456);
+            this.textBox_vypis_telefon.Name = "textBox_vypis_telefon";
+            this.textBox_vypis_telefon.Size = new System.Drawing.Size(211, 20);
+            this.textBox_vypis_telefon.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(68, 502);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(192, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Celková cena uhrazená za průkaz ISIC";
+            // 
+            // textBox_vypis_celkova_cena
+            // 
+            this.textBox_vypis_celkova_cena.Location = new System.Drawing.Point(297, 495);
+            this.textBox_vypis_celkova_cena.Name = "textBox_vypis_celkova_cena";
+            this.textBox_vypis_celkova_cena.Size = new System.Drawing.Size(102, 20);
+            this.textBox_vypis_celkova_cena.TabIndex = 23;
+            // 
+            // button_register
+            // 
+            this.button_register.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_register.Location = new System.Drawing.Point(406, 492);
+            this.button_register.Name = "button_register";
+            this.button_register.Size = new System.Drawing.Size(102, 23);
+            this.button_register.TabIndex = 24;
+            this.button_register.Text = "REGISTROVAT";
+            this.button_register.UseVisualStyleBackColor = true;
+            this.button_register.Click += new System.EventHandler(this.button_register_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(297, 531);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(240, 17);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "Souhlasím s podmínkami poskytování služeb";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(59, 330);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(73, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Cena průkazu";
+            // 
+            // textBox_cena2
+            // 
+            this.textBox_cena2.Location = new System.Drawing.Point(138, 327);
+            this.textBox_cena2.Name = "textBox_cena2";
+            this.textBox_cena2.Size = new System.Drawing.Size(130, 20);
+            this.textBox_cena2.TabIndex = 27;
+            this.textBox_cena2.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(281, 330);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(89, 13);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "Pojištění průkazu";
+            // 
+            // textBox_pojisteni2
+            // 
+            this.textBox_pojisteni2.Location = new System.Drawing.Point(376, 327);
+            this.textBox_pojisteni2.Name = "textBox_pojisteni2";
+            this.textBox_pojisteni2.Size = new System.Drawing.Size(102, 20);
+            this.textBox_pojisteni2.TabIndex = 29;
+            this.textBox_pojisteni2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 573);
+            this.Controls.Add(this.textBox_pojisteni2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox_cena2);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button_register);
+            this.Controls.Add(this.textBox_vypis_celkova_cena);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox_vypis_telefon);
+            this.Controls.Add(this.textBox_vypis_cele_jmeno);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button_castka);
+            this.Controls.Add(this.textBox_pojisteni);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_cena);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button_jmeno_prijmeni);
+            this.Controls.Add(this.textBox_prijmeni);
+            this.Controls.Add(this.textBox_telefon);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.textBox_jmeno);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_jmeno;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_telefon;
+        private System.Windows.Forms.TextBox textBox_prijmeni;
+        private System.Windows.Forms.Button button_jmeno_prijmeni;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_cena;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_pojisteni;
+        private System.Windows.Forms.Button button_castka;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox_vypis_cele_jmeno;
+        private System.Windows.Forms.TextBox textBox_vypis_telefon;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox_vypis_celkova_cena;
+        private System.Windows.Forms.Button button_register;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox_cena2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox_pojisteni2;
+    }
+}
+
---
+++ isic/isic/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace isic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_jmeno_prijmeni_Click(object sender, EventArgs e)
+        {
+            string jmeno;
+            jmeno = Convert.ToString(textBox_jmeno.Text);
+
+            string prijmeni;
+            prijmeni = Convert.ToString(textBox_prijmeni.Text);
+
+            string souhrn1;
+            souhrn1 = jmeno + " " + prijmeni;
+
+            textBox_vypis_cele_jmeno.Text = Convert.ToString(souhrn1);
+
+            string telefon;
+            telefon = Convert.ToString(textBox_telefon.Text);
+
+            textBox_vypis_telefon.Text = Convert.ToString(telefon);
+        }
+
+        private void button_castka_Click(object sender, EventArgs e)
+        {
+            int cena;
+            cena = Convert.ToInt32(textBox_cena.Text);
+
+            int pojisteni;
+            pojisteni = Convert.ToInt32(textBox_pojisteni.Text);
+
+            int cena2;
+            cena2 = Convert.ToInt32(textBox_cena2.Text);
+
+            int pojisteni2;
+            pojisteni2 = Convert.ToInt32(textBox_pojisteni2.Text);
+
+            int souhrn2;
+            souhrn2 = cena + pojisteni + cena2 + pojisteni2;
+
+            textBox_vypis_celkova_cena.Text = Convert.ToString(souhrn2);
+        }
+
+        private void button_register_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Registrace proběhla v pořádku, děkujeme.");
+
+            if (textBox_vypis_cele_jmeno.Text != " " && textBox_vypis_telefon.Text != " " && textBox_vypis_celkova_cena.Text != " " && checkBox1.Checked == true)
+                if (MessageBox.Show("Chcete opravdu provést registraci?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                else
+                {
+                    this.Close();
+                }
+
+            else
+                MessageBox.Show("Registrace neproběhla v pořádku.");
+        }
+
+    }
+}
